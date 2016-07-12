8f4815c278b231d051320c5b00140203bdccec75
Agustin Camino <agustin.camino@gmail.com>
2016-07-12 13:56:01 -0500

    Add reset button functionality

---
+++ public/javascripts/notify.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  Notify.init();
+});
+
+var Notify = {
+  init: function () {
+    var self = this;
+    $('#reset').click(function(e) {
+      e.preventDefault();
+      self.reset();
+    });
+  },
+
+  reset: function () {
+    $('textarea[name=message]').val('');
+    $('select[name=movie]').prop('selectedIndex', 0);
+  }
+};
+
